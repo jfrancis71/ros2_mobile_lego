cdf1ba88bd058ac347a4f3f3145f0f49397e6243
Julian Francis <julian.w.francis@gmail.com>
2025-03-29 08:54:25 +0000

    Fixing and updating launch and config files.

--- thomas/launch/brickpi3_motors_launch.py
+++ thomas/launch/brickpi3_motors_launch.py
@@ -10,40 +10,22 @@ def generate_launch_description():
         PathJoinSubstitution(
             [FindPackageShare("thomas"), "config", "robot_hardware_description.urdf"])
         ])
-
     robot_controllers = PathJoinSubstitution(
-            [FindPackageShare("thomes"), "config", "robot_description.yaml"])
-
+            [FindPackageShare("thomas"), "config", "robot_description.yaml"])
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_controllers],
+        parameters=[{"robot_description": robot_description_content}, robot_controllers],
+        remappings=[("/differential_drive_controller/cmd_vel_unstamped", "/cmd_vel")],
         output="both",
     )
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=[
-            "omni_wheel_controller",
-            "--param-file",
-            robot_controllers,
-            "--controller-ros-args",
-            "-r /omni_wheel_controller/cmd_vel:=/cmd_vel",
-        ],
+        arguments=["differential_drive_controller", "--controller-manager", "/controller_manager"],
     )
-
-    robot_description = {"robot_description": robot_description_content}
-
-    robot_state_pub_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[robot_description],
-    )
-
     nodes = [
         control_node,
-        robot_state_pub_node,
         robot_controller_spawner
     ]
 
