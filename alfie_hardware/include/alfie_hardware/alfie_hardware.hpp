6ac59505e2f2addc5284206b5d3e72207504d4e3
Julian Francis <julian.w.francis@gmail.com>
2025-08-10 09:04:17 +0100

    Removing visibility control, we don't need it. I'm only supporting Linux distributions.

--- alfie_hardware/include/alfie_hardware/alfie_hardware.hpp
+++ alfie_hardware/include/alfie_hardware/alfie_hardware.hpp
@@ -16,7 +16,6 @@
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 
-#include "alfie_hardware/visibility_control.h"
 
 namespace alfie_hardware
 {
@@ -25,29 +24,22 @@ class AlfieHardware : public hardware_interface::SystemInterface
 public:
   RCLCPP_SHARED_PTR_DEFINITIONS(AlfieHardware);
 
-  ALFIE_HARDWARE_PUBLIC
   hardware_interface::CallbackReturn on_init(
     const hardware_interface::HardwareInfo & info) override;
 
-  ALFIE_HARDWARE_PUBLIC
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
-  ALFIE_HARDWARE_PUBLIC
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
-  ALFIE_HARDWARE_PUBLIC
   hardware_interface::CallbackReturn on_activate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  ALFIE_HARDWARE_PUBLIC
   hardware_interface::CallbackReturn on_deactivate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  ALFIE_HARDWARE_PUBLIC
   hardware_interface::return_type read(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-  ALFIE_HARDWARE_PUBLIC
   hardware_interface::return_type write(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
